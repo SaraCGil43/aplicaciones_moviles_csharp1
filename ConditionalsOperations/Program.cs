8210ad0310e69ae5b55f5ca68db46dd9a8e9feaf
SaraCGil43 <saritacolgil23@gmail.com>
2024-03-19 22:34:53 -0500

    practice about conditionals

---
+++ ConditionalsOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Conditionals on C#
+Console.WriteLine("Escribe un número:");
+int num = int.Parse(Console.ReadLine());
+
+if(num >0){
+    Console.WriteLine("El número "+num +" es positivo");
+}else{
+ Console.WriteLine("El número "+num +" es negativo");
+}
+
+
