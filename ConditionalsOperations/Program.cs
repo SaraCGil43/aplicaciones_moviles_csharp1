84473bb472d65d1e41142f3a546ddef128aaf127
SaraCGil43 <saritacolgil23@gmail.com>
2024-03-19 22:40:40 -0500

    Condicional exercise 2

--- ConditionalsOperations/Program.cs
+++ ConditionalsOperations/Program.cs
@@ -9,4 +9,10 @@ if(num >0){
  Console.WriteLine("El número "+num +" es negativo");
 }
 
+Console.WriteLine("Escribe el primer número:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Escribe el segundo número:");
+double b = double.Parse(Console.ReadLine());
+const string Indeterminado = "Indeterminado";
 
+Console.WriteLine($"Suma: {a+b}, Resta:  {a-b}, Multiplicación: {a*b}, Divisón {(b!=0 ? a/b : Indeterminado)}");
